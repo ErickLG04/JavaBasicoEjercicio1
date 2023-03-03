1fef97360769cc716902e0e72e198142a1eb4b3b
ErickLG04 <erickjavierlg@gmail.com>
2023-03-02 22:01:20 -0500

    Agregar Proyecto

---
+++ src/Ejercicio1/Main.java
@@ -0,0 +1,23 @@
+package Ejercicio1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numero = 9;
+        long numero2 = 564;
+        double numero3 = 123.456;
+        boolean no = false;
+        boolean si = true;
+        String nombre = "erick";
+
+        System.out.println(numero);
+        System.out.println(numero2);
+        System.out.println(no);
+        System.out.println(si);
+        System.out.println(nombre);
+
+
+
+    }
+}
